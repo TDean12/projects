b5ab4f510ff45672b7f0e645e743b0b371e95a79
Tuuguu_dean <tuuguu_dean@nest-52s-iMac.local>
2023-02-04 10:32:35 +0800

    du

---
+++ bogmy/backend/test.js
@@ -0,0 +1,33 @@
+// beforeAll(() => {
+//     console.log("im logging before all");
+// });
+
+// afterAll(async () => {
+//     console.log("im logging out after all");
+// });
+
+
+// const helloName = (name) => {
+//     return `Hello ${name}`
+// }
+
+// describe("Our very first test", () => {
+//     it("First taste case", async () => {
+//         const a = 2
+//         const b = "4"
+//         const c = 7
+//         console.log("im the logging of first case");
+//         const result = a + b + c
+//         expect(result).toBe("247")
+//     });
+
+//     it("Second taste case", async () => {
+//         const result = helloName("Bat")
+//         expect(result).toBe("Hello Bat")
+//     });
+
+//     it("Third taste case", async () => {
+//         const result = helloName("Bat")
+//         expect(result).toBe("Hello Nest");
+//     })
+// })
